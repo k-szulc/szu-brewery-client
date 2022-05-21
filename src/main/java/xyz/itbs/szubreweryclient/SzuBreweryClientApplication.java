d8cf6e7e8b353e0e25494149a0f1da1595e4dd1e
kamil_szulc <kamil.szulc@logintegra.com>
2022-05-21 13:24:22 +0200

    initial commit

---
+++ src/main/java/xyz/itbs/szubreweryclient/SzuBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package xyz.itbs.szubreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzuBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzuBreweryClientApplication.class, args);
+	}
+
+}
